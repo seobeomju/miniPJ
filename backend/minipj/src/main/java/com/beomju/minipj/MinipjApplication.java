a6c7634f9129810e04a863fd344832470f45e5cb
seobeomju <88228101+seobeomju@users.noreply.github.com>
2025-04-17 14:27:11 +0900

    전체 프로젝트 초기 추가

---
+++ backend/minipj/src/main/java/com/beomju/minipj/MinipjApplication.java
@@ -0,0 +1,13 @@
+package com.beomju.minipj;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MinipjApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MinipjApplication.class, args);
+	}
+
+}
---
+++ backend/minipj/src/test/java/com/beomju/minipj/MinipjApplicationTests.java
@@ -0,0 +1,13 @@
+package com.beomju.minipj;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MinipjApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
